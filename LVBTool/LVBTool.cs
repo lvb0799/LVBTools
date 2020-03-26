b47b48073b8f52c0eab61e35fba73470a8fce253
lvb0799 <lighthouse07992000@yahoo.com.tw>
2020-03-27 00:13:52 +0800

    addToolProject

---
+++ LVBTool/LVBTool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LVBTool
+{
+    public partial class LVBTool : Form
+    {
+        public LVBTool()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SQLToPG_Click(object sender, EventArgs e)
+        {
+            this.txt_result.Text = string.Empty;
+            string[] oriStr = txt_ori.Lines;
+            string objName = this.txt_Var.Text.Trim();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"StringBuilder {objName} = new StringBuilder();");
+            foreach (string s in oriStr)
+            {
+                sb.AppendLine($"{ objName}.AppendLine(\"{s}\");");
+            }
+            this.txt_result.Text = sb.ToString();
+        }
+
+        private void btn_PGToSQL_Click(object sender, EventArgs e)
+        {
+            this.txt_result.Text = string.Empty;
+            string[] oriStr = txt_ori.Lines;
+            string objName = this.txt_Var.Text.Trim();
+            StringBuilder sb = new StringBuilder();
+            //sb.AppendLine($"StringBuilder {objName} = new StringBuilder();");
+            //if(oriStr.Length>0 && oriStr[0].Contains("new StringBuilder()"))
+
+            foreach (string s in oriStr)
+            {
+                if (!s.Contains("new StringBuilder()"))
+                {
+                    //sb.AppendLine($"{ objName}.AppendLine(\"{s}\");");
+                    sb.AppendLine($"{s.Replace($"{objName}.AppendLine(\"",string.Empty).Replace("\");",string.Empty)}");
+                }
+            }
+            this.txt_result.Text = sb.ToString();
+        }
+    }
+}
